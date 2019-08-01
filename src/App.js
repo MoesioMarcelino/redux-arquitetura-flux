63828c93fe9374ef48d08fe1eb86c6d46d5c994a
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-08-01 12:01:03 -0300

    routes and components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
-  return <p>Hello Redux</p>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;

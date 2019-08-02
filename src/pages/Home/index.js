cb12be8528bcaea062332e2faea0d2465ee05e9e
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-08-01 22:22:27 -0300

    add to cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import { connect } from 'react-redux';
 
 import api from '../../services/api';
 
 import { formatPrice } from '../../util/format';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -22,6 +23,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -32,7 +42,10 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -44,3 +57,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);

2a608f7236a55a2558e89afd9fb31ba0c46fc138
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-08-01 21:51:50 -0300

    search data in the API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,96 +1,45 @@
 import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
-
 import { ProductList } from './styles';
 
+import api from '../../services/api';
+
+import { formatPrice } from '../../util/format';
+
 export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?resize=326:*"
-            alt="Sapato"
-          />
-          <strong>Tênis muito legal</strong>
-          <span>R$ 129,90</span>
-          <button>
-            <div>
-              <MdShoppingCart size={16} color="#FFF" /> 3
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button>
+              <div>
+                <MdShoppingCart size={16} color="#FFF" /> 3
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
       </ProductList>
     );
   }
